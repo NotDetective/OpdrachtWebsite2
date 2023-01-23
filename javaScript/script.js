1a990cc5b832c1862bfdce0a8c54927c7aca3722
MichaZonderS <micha-elmans@outlook.com>
2023-01-23 15:31:14 +0100

    over mij page finish

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,14 +3,14 @@ function aboutMePageSwitch() {
   if (document.getElementById("show").style.display == "none") {
     document.getElementById("show").style.display = 'block';
     document.getElementById("show2").style.display = 'block';
-    // document.getElementById("hidden").style.display = 'none';
+    document.getElementById("hidden").style.display = 'none';
     document.getElementById("hidden2").style.display = 'none';
     document.getElementById("hidden3").style.display = 'none';
     document.getElementById("hidden4").style.display = 'none';
   } else{
     document.getElementById("show").style.display = 'none';
     document.getElementById("show2").style.display = 'none';
-    // document.getElementById("hidden").style.display = 'block';
+    document.getElementById("hidden").style.display = 'block';
     document.getElementById("hidden2").style.display = 'block';
     document.getElementById("hidden3").style.display = 'block';
     document.getElementById("hidden4").style.display = 'block';
